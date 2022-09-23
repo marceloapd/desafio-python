8f19db27d109772d857d39992b881fb72903b75c
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-09-23 02:53:41 -0300

    create auth service

---
+++ desafio_python/services/auth.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException
+
+import jwt
+import time
+import os
+
+SECRET_JWT = os.getenv('SECRET_JWT')
+
+class AuthService:
+    def __init__(self):
+        pass
+
+    def generate_token(self):
+        return jwt.encode(
+            {"payload": "047a9dd00a76b8237094f519fa08dbc0",
+            "exp": int(time.time() + 3600),},
+            SECRET_JWT,
+            algorithm="HS256",
+        )
+    
+    def verify_token(self, token):
+        try:
+            payload = jwt.decode(token, SECRET_JWT, ["HS256"])
+        except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, jwt.DecodeError):
+            raise HTTPException(403, 'Token error')
+        except Exception as e:
+            raise HTTPException(500,'Server error')
