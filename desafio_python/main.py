741ca48c4305b38fa5a139c0b9270ef957a58b2c
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-09-23 02:54:34 -0300

    include dependecie to check tokens

--- desafio_python/main.py
+++ desafio_python/main.py
@@ -1,13 +1,14 @@
 from fastapi import Depends, FastAPI
 
 from desafio_python.dependencies.databases import get_postgres_session
+from desafio_python.dependencies.auth import check_token
 from .routers import token, user
 from .services.import_jsonplaceholder import ImportJsonplaceholder
 
 app = FastAPI()
 
 app.include_router(token.router, tags=["get_token"], prefix="/auth")
-app.include_router(user.router, tags=["get_websites"], prefix="/users")
+app.include_router(user.router, tags=["get_websites"], prefix="/users", dependencies=[Depends(check_token)])
 
 @app.on_event("startup")
 async def seed_db_event():
