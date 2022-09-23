59a6005cfaad3608bcf2922f3bcc0c064eb8b940
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-09-23 01:45:25 -0300

    include routes in main

--- desafio_python/main.py
+++ desafio_python/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+
+from desafio_python.dependencies.databases import get_postgres_session
+from .routers import token, user
+from .services.import_jsonplaceholder import ImportJsonplaceholder
 
 app = FastAPI()
 
+app.include_router(token.router, tags=["get_token"], prefix="/auth")
+app.include_router(user.router, tags=["get_websites"], prefix="/users")
+
+@app.on_event("startup")
+async def seed_db_event():
+    postgres = get_postgres_session()
+    user = ImportJsonplaceholder(postgres)
+    return user.get_jsonplaceholder()
 
 @app.get("/")
 def index():
-    return "Hello world!"
+    return "Hello, world!"
