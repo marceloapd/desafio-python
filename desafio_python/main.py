a2a104c2eb1dc0fddd41333de92ae7c7a71913e6
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-09-26 00:44:05 -0300

    reorganize main.py

--- desafio_python/main.py
+++ desafio_python/main.py
@@ -4,17 +4,28 @@ from desafio_python.dependencies.databases import get_postgres_session
 from desafio_python.dependencies.auth import check_token
 from .routers import token, user
 from .services.import_jsonplaceholder import ImportJsonplaceholder
+import requests
 
 app = FastAPI()
 
 app.include_router(token.router, tags=["get_token"], prefix="/auth")
-app.include_router(user.router, tags=["get_websites"], prefix="/users", dependencies=[Depends(check_token)])
+app.include_router(
+    user.router,
+    tags=["get_websites"],
+    prefix="/users",
+    dependencies=[Depends(check_token)],
+)
+
 
 @app.on_event("startup")
 async def seed_db_event():
+    api_response = requests.get("https://jsonplaceholder.typicode.com/users")
+    if api_response.status_code != 200:
+        raise ValueError("")
     postgres = get_postgres_session()
     user = ImportJsonplaceholder(postgres)
-    return user.get_jsonplaceholder()
+    return user.get_jsonplaceholder(api_response.json())
+
 
 @app.get("/")
 def index():
