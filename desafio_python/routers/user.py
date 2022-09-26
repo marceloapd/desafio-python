825b924a124b108d88c7597c02c60928a9ab879c
Marcelo Assis <71731452+marceloapd@users.noreply.github.com>
2022-09-26 01:07:06 -0300

    not allow empty searches

--- desafio_python/routers/user.py
+++ desafio_python/routers/user.py
@@ -21,5 +21,7 @@ def get_details(user_service: UserService = Depends(get_user_service)):
 
 @router.get("/", status_code=200)
 def get_user_by_name(name: str, user_service: UserService = Depends(get_user_service)):
+    if not name:
+        return "Invalid username."
     user_by_name = user_service.get_user_by_name(name)
     return user_by_name
